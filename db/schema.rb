49ef8f0d761154a39c4682cd5a38026a2afb3f4b
andrewlloyd0129 <andrewlloyd0129@gmail.com>
2018-04-12 12:44:20 -0600

    put crud in controller. adding views

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20180412165249) do
+ActiveRecord::Schema.define(version: 20180412182822) do
 
   create_table "donuts", force: :cascade do |t|
     t.integer "dough", default: 0
@@ -19,7 +18,9 @@ ActiveRecord::Schema.define(version: 20180412165249) do
     t.integer "topping", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
+    t.float "price"
+    t.string "title"
+  end
 
   create_table "votes", force: :cascade do |t|
     t.string "votable_type"
