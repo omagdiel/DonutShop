a6515f35e532bfa57f4340b0bd53279361a31be1
Jukinner <jukinner@gmail.com>
2018-04-12 13:50:15 -0600

    Implemented Stripe basics

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  resources :charges, only: [:new, :create]
+  # get 'charges/new'
+
+  # get 'charges/create'
+
   devise_for :users
   resources :donuts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  
+  root to: 'donuts#index'
 end
