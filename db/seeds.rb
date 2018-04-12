fbb72ee1370705c35bc078d4d76b66eb870f03d0
omagdiel <omagdiel@gmail.com>
2018-04-12 17:18:34 -0600

    final refactors before app in a day presentation.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@ User.create!(
    email: "admin@test.com",
    password: "asdfasdf",
    password_confirmation: "asdfasdf",
+   roles: "site_admin"
 )
 
 puts "1 admin user created"
@@ -21,9 +22,6 @@ Donut.create!(title: "glazed", dough: 0, frosting: 0, price: 0.99)
 
 Donut.create!(title: "maple", dough: 1, frosting: 1, price: 0.99)
 
-Donut.create!(title: "triple chocolate", dough: "chocolate", frosting: "chocolate_frosting", topping: "chocolate_chips", price: 1.39)
-
 Donut.create!(title: "sprinkles", dough: 1, frosting: 0, topping: 0, price: 1.25)
 
 Donut.create!(title: "maple bacon", dough: 1, frosting: 1, topping: 1, price: 1.99)
-
