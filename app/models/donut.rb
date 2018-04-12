cbdcc56cdf7d53436253c6a126f020646b2aa896
andrewlloyd0129 <andrewlloyd0129@gmail.com>
2018-04-12 13:06:59 -0600

    created index for donuts and basic donuts in seeds

--- app/models/donut.rb
+++ app/models/donut.rb
@@ -1,2 +1,5 @@
 class Donut < ApplicationRecord
+  enum dough: [:regular, :cake, :chocolate, :chocolate_cake, :strawberry, :vegan]
+  enum frosting: [:glazed, :maple, :chocolate_frosting, :strawberry_frosting]
+  enum topping: [:sprinkles, :bacon, :chocolate_chips, :oreo, :candy, :cookie]
 end
